08103221c6e250a490219c3793a1e5aed5ac0b37
Taylor Mills <taylor.mills@centene.com>
2020-11-23 13:50:55 -0500

    rename classes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 
 const App = () => {
   return (
-    <div className='pageContainer'>
+    <div className='app'>
       <div className='header'>
         <Header />
       </div>
