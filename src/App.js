9fe1b43e01222d7a6a74c4b64ddf2adb46162b62
Taylor Mills <taylor.mills@centene.com>
2020-11-23 12:38:18 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { Form, Button, TextArea, Icon } from 'semantic-ui-react'
+import Home from './Components/Home/Home'
 import classes from './CSS/Main.module.css'
 
 const App = () => {
   return (
     <div className={classes.pageContainer}>
-      <Form className={classes.form}>
-         <TextArea style={{ width: '30%', resize: 'none' }} />
-        <div className={classes.buttonContainer}>
-          <Button color='green' className={classes.button}>Mock</Button>
-          <Icon name='long arrow alternate right' size='huge' />
-          <Button color='green' basic className={classes.button}>Reset</Button>
-        </div>
-         <TextArea style={{ width: '30%', resize: 'none' }} />
-      </Form>
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Form, Button, TextArea, Icon} from 'semantic-ui-react'
+
+const Home = () => {
+    return (
+        <Form className={classes.form}>
+        <div className={classes.textAreaContainer}>
+          <label >Input</label>
+          <TextArea style={{ width: '100%', height: '100%', resize: 'none' }} />
+        </div>
+        <div className={classes.buttonContainer}>
+          <Button color='green' className={classes.button}>Mock</Button>
+          <Icon name='long arrow alternate right' size='huge' />
+          <Button color='green' basic className={classes.button}>Reset</Button>
+        </div>
+        <div className={classes.textAreaContainer}>
+          <label>Output</label>
+          <TextArea style={{ width: '100%', height: '100%', resize: 'none' }} />
+        </div>
+      </Form>
+    )
+}
+
+export default Home
