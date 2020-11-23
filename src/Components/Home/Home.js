276aca70c5696ce461df57b6b018a0e3c1ed393c
David Dixon <david.dixon@centene.com>
2020-11-23 10:36:06 -0800

    added icon8 link

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -22,7 +22,19 @@ const Home = () => {
             <TextArea style={{ width: '100%', height: '100%', resize: 'none' }} />
           </div>
         </Form>
-      </div>
+        <div style={{
+          position: "absolute",
+          bottom: "0",
+          width: "100%",
+          textAlign: "center",
+          color: "#9e9ec9"
+          }}>Bird icon courtesy of <a
+            href="icons8.com"
+            target="_blank"
+            style={{color: "#9e9ec9"}}
+            >icons8.com</a></div>
+        </div>
+
     )
 }
 
