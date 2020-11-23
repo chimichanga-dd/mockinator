398a197ea94f02cbe0bea5cd93f81aa4e763c194
Taylor Mills <taylor.mills@centene.com>
2020-11-23 14:03:53 -0500

    Add footer component

---
+++ src/components/shared/footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div style={{
+            position: "absolute",
+            bottom: "0",
+            width: "100%",
+            textAlign: "center",
+            color: "#9e9ec9"
+            }}>Bird icon courtesy of <a
+              href="icons8.com"
+              target="_blank"
+              style={{color: "#9e9ec9"}}
+              >icons8.com</a>
+        </div>
+    )
+}
+
+export default Footer
