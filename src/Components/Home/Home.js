f48804acdc33bd2918c9a2de4019cb653c30fc99
Taylor Mills <taylor.mills@centene.com>
2020-11-23 14:10:56 -0500

    remove

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -35,19 +35,7 @@ const Home = () => {
             <TextArea/>
           </div>
         </Form>
-        <div style={{
-          position: "absolute",
-          bottom: "0",
-          width: "100%",
-          textAlign: "center",
-          color: "#9e9ec9"
-          }}>Bird icon courtesy of <a
-            href="icons8.com"
-            target="_blank"
-            style={{color: "#9e9ec9"}}
-            >icons8.com</a></div>
         </div>
-
     )
 }
 
