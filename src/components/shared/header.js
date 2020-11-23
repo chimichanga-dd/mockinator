23145f44224666f01deb9f14b14864cfa7b1a49d
Taylor Mills <taylor.mills@centene.com>
2020-11-23 14:13:01 -0500

    added files

---
+++ src/components/shared/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div style={{display: 'flex'}}>
+            <h2>Mockinator</h2>
+            {/* Logo here maybe? */}
+        </div>
+    )
+}
+
+export default Header
